c4e6cd939c35a934fb9fce26d0a3d0d5f70be135
Ashish Sheoran <sheoranashu079@gmail.com>
2021-03-19 08:21:46 -0400

    Added Login functionality

--- chat-clone/src/App.js
+++ chat-clone/src/App.js
@@ -2,15 +2,19 @@ import React from 'react';
 import { ChatEngine } from 'react-chat-engine';
 import './App.css';
 import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm'
 
 const App = () => {
+
+  if (!localStorage.getItem('username')) return <LoginForm />
+
   return (
-    <ChatEngine 
+    <ChatEngine
       height="100vh"
       projectID="ace849b7-f144-4909-9008-ae03ba0fa906"
       userName="Ashish"
       userSecret="123123"
-      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} /> }
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
     />
   )
 }
---
+++ chat-clone/src/components/LoginForm.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+const LoginForm = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        // Enter username / password => chatEngine => give messages
+        const authObject = { 'Project-ID': 'ace849b7-f144-4909-9008-ae03ba0fa906', 'User-Name': username, 'User-Secret': password }
+
+        try {
+            await axios.get('https://api.chatengine.io/chats', { headers: authObject });
+
+            // works out -> logged in
+            localStorage.setItem('username', username);
+            localStorage.setItem('password', password);
+
+            window.location.reload();
+        } catch (error) {
+            // doesn't work / error -> try with new username...
+        }
+    }
+
+    return (
+        <div className="wrapper">
+            <div className="form">
+                <h1 className="title">Chat Application</h1>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="input" placeholder="Username" required/>
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="input" placeholder="Password" required/>
+                    <div align="center">
+                        <button type="submit" className="button">
+                            <span>Start Chatting</span>
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default LoginForm;
