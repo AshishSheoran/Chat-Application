e53cb6ded990601dba642bd9f7044a31082821ca
Ashish Sheoran <sheoranashu079@gmail.com>
2021-03-19 04:01:37 -0400

    Added Message Form Template

--- chat-clone/src/components/MessageForm.jsx
+++ chat-clone/src/components/MessageForm.jsx
@@ -1,10 +1,28 @@
 // From used for sending messges
+import { useState } from 'react';
+
 
 const MessageForm = () => {
+    const [value, setValue ] = useState('');
+
+    const handleSubmit = () => {
+
+    }
+
+    const handleChange = () => {
+
+    }
+
     return (
-        <div>
-            MessageForm
-        </div>
+        <form className="message-form" onSubmit={handleSubmit}>
+            <input 
+                className="message-input"
+                placeholder="Send a message ..."
+                value={value}
+                onChange={handleChange}
+                onSubmit={handleSubmit}
+            />
+        </form>
     )
 } 
 
