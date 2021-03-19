58d4ed884d3cbbdb4a8da20dc5d79ff6e1892a97
Ashish Sheoran <sheoranashu079@gmail.com>
2021-03-19 03:31:12 -0400

    Created Functionality for chat feeds

--- chat-clone/src/components/ChatFeed.jsx
+++ chat-clone/src/components/ChatFeed.jsx
@@ -4,11 +4,53 @@ import MyMessage from './MyMessage';
 import TheirMessage from './TheirMessage';
 import MessageForm from './MessageForm';
 
-const chatFeed = (props) => {
+const ChatFeed = (props) => {
+    const { chats, activeChat, userName, message } = props;
+
+    // If chats exist, then assign them to chat.
+    const chat = chats && chats[activeChat];
+
+    const renderMessages = () => {
+        // Take keys(id's) from messages and then assign them to keys.
+        const keys = Object.keys(messages);
+
+        return keys.map((key, index) => {
+            const message = messages[key];
+            const lastMessageKey = index === 0 ? null : keys[index - 1];
+            const isMyMessage = userName === message.sender.userName;
+
+            return (
+                <div key={`msg_${index}`} style={{width: '100%'}}>
+                    <div className="message-block">
+                        {
+                            isMyMessage
+                            ? <MyMessage message={message}/>
+                            : <TheirMessage message={message} lastMessageKey={messages[lastMessageKey]} />
+                        }
+                    </div>
+                    <div className="read-receipts" style={{ marginRight: myMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px' : '68px'}}>
+                        read-receipts
+                    </div>
+                </div>
+            );
+        })
+    }
+
+    if(!chat) return 'Loading...';
     
     return (
-        <div>
-            ChatFeed
+        <div className="chat-feed">
+            <div className="chat-title-container">
+                <div className="chat-title">{chat?.title}</div>     // "chat?" will make sure we have a chat before assigning title
+                <div className="chat-subtitle">
+                    {chat.people.map((person) => ` ${person.person.userName}`)}
+                </div>
+            </div>
+            {renderMessage()}
+            <div style={{height: '100px'}} />
+            <div className="message-form-container">
+                <MessageForm {...props} chatId={activeChat} />
+            </div>
         </div>
     )
 }
