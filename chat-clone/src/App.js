914713b53a5c37fedeb163b9b268b03a1c55960c
Ashish Sheoran <sheoranashu079@gmail.com>
2021-03-19 03:01:07 -0400

    Added Chat Feed component

--- chat-clone/src/App.js
+++ chat-clone/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ChatEngine } from 'react-chat-engine';
 import './App.css';
+import ChatFeed from './components/ChatFeed';
 
 const App = () => {
   return (
@@ -9,7 +10,7 @@ const App = () => {
       projectID="ace849b7-f144-4909-9008-ae03ba0fa906"
       userName="Admin"
       userSecret="123123"
-
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} /> }
     />
   )
 }
